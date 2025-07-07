3539b2aa23653822d1e520850dc83520955e7543
petaHarsha <155061879+petaHarsha@users.noreply.github.com>
2025-07-07 13:31:04 +0530

    Update game_controller.py

--- src/game_controller.py
+++ src/game_controller.py
@@ -1,1 +1,28 @@
+import json
+import pyautogui
 
+class GameController:
+    def __init__(self, config_path="config/key_bindings.json"):
+        with open(config_path) as f:
+            self.key_bindings = json.load(f)
+        self.current_keys = set()
+
+    def press_key(self, key):
+        if key not in self.current_keys:
+            pyautogui.keyDown(key)
+            self.current_keys.add(key)
+
+    def release_key(self, key):
+        if key in self.current_keys:
+            pyautogui.keyUp(key)
+            self.current_keys.remove(key)
+
+    def release_all_keys(self):
+        for key in list(self.current_keys):
+            self.release_key(key)
+
+    def mouse_action(self, action="up"):
+        if action == "down":
+            pyautogui.mouseDown(button=self.key_bindings["shoot"])
+        else:
+            pyautogui.mouseUp(button=self.key_bindings["shoot"])
